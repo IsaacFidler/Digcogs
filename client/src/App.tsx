8dd8d31f29f9c954784603404ff605102c91cf33
IsaacFidler <zac.fidler2@gmail.com>
2021-11-23 23:18:02 +0000

    feat: added boostrap, added header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth0ProviderWithHistory from './auth0provider';
+
+
+//components
+
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to issssaaaac.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0ProviderWithHistory>
+      <div className="App">
+        <Header/>
+      </div>
+    </Auth0ProviderWithHistory>
   );
 }
 
