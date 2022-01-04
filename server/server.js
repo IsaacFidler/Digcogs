b86d3b2f3c4efc7a661ab2608935669b4161e244
IsaacFidler <zac.fidler2@gmail.com>
2022-01-04 18:52:49 +0000

    feat: oAuth working, redirection not. Getting user information correctly after authenticating through discogs

--- server/server.js
+++ server/server.js
@@ -22,22 +22,51 @@ app.get('/', (req, res) => {
 
 app.use(cors())
 
+let myRequestData;
+let myAccessData;
+
+
 app.get('/authorize', function (req, res) {
+  console.log('yes')
   var oAuth = new Discogs().oauth();
   oAuth.getRequestToken(
     'ynCailbhySoeBgjSbiuR',
     'sYjPnEQSPdwqqZqwMChoUCTnKvLRJxiU',
-    'http://localhost:3000/dashboard',
+    'http://localhost:9000/callback',
     function (err, requestData) {
       // Persist "requestData" here so that the callback handler can
       // access it later after returning from the authorize url
+      myRequestData = requestData
+      console.log(myRequestData)
       res.redirect(requestData.authorizeUrl);
     }
   );
 });
-// app.get('/login/discogs/callback', (req, res) => {
-//   res.send('hellloo')
-// })
+
+
+
+app.get('/callback', function (req, res) {
+  var oAuth = new Discogs(myRequestData).oauth();
+  oAuth.getAccessToken(
+    req.query.oauth_verifier, // Verification code sent back by Discogs
+    function (err, accessData) {
+      // Persist "accessData" here for following OAuth calls
+      myAccessData = accessData
+      console.log(myAccessData)
+      res.send('Received access token!');
+    }
+  );
+});
+
+app.get('/identity', function (req, res) {
+  console.log(myAccessData)
+  var dis = new Discogs(myAccessData);
+
+  dis.getIdentity(function (err, data) {
+    console.log(data)
+    res.send(data);
+  });
+});
 
 const PORT = 9000
 
